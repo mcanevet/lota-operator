9b72aaf74e3a6667e4553a25093b8d71aa138374
Mickaël Canévet <mickael.canevet@camptocamp.com>
2019-12-08 11:01:47 +0100

    Remove broken code

--- pkg/controller/lotaprovider/lotaprovider_controller.go
+++ pkg/controller/lotaprovider/lotaprovider_controller.go
@@ -15,9 +15,7 @@ import (
 	apiextensionsclientset "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -121,19 +119,6 @@ func add(mgr manager.Manager, r reconcile.Reconciler, client dynamic.Interface) 
 	})
 	if err != nil {
 		return err
-	}
-
-	// Watch for changes to GVKs relevant for LotaProvider
-	gvks := []schema.GroupVersionKind{
-		{Group: "lota-provider.io", Version: "", Kind: ""},
-	}
-	for _, gvk := range gvks {
-		u := &unstructured.Unstructured{}
-		u.SetGroupVersionKind(gvk)
-		myController.Controller.Watch(&source.Kind{Type: u}, &handler.EnqueueRequestForOwner{
-			IsController: true,
-			OwnerType:    &lotaproviderv1alpha1.LotaProvider{},
-		})
 	}
 
 	return nil
